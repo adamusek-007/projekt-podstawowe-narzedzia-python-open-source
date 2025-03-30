731ae3e371f3e805cd303269dd83afdb3bbd908d
adamusek-007 <adam.kuczabinski@gmail.com>
2025-03-30 18:06:25 +0200

    Create Config.py class file

---
+++ project/Config.py
@@ -0,0 +1,14 @@
+class Config:
+    def __init__(
+        self,
+        imap_server_address,
+        imap_email_username,
+        imap_email_password,
+        imap_port,
+        output_csv_filename,
+    ):
+        self.imap_server_address = imap_server_address
+        self.imap_email_username = imap_email_username
+        self.imap_email_password = imap_email_password
+        self.imap_port = imap_port
+        self.output_csv_filename = output_csv_filename
