9deab09a106654f7f8e44d2ab68616e573915f2d
adamusek-007 <adam.kuczabinski@gmail.com>
2025-03-30 19:08:54 +0200

    Refactor flask app

--- project/main.py
+++ project/main.py
@@ -9,29 +9,28 @@ from console_configurator import ConsoleConfigurator
 from configuration_way import ConfigurationWay
 from email_address_exporter import EmailAddressExporter
 
-# Choose from user that either he want to supply programm by console or by the web
-
 app = Flask(__name__)
-exporter = EmailAddressExporter()
 
 
 @app.route("/", methods=["GET", "POST"])
 def configure():
-    global exporter
+    """Display configuration site, and on POST colletcts data"""
     if request.method == "POST":
-        exporter = EmailAddressExporter(
-            request.form["imap_server_address"],
-            request.form["imap_email_username"],
-            request.form["imap_email_password"],
-            int(request.form["imap_port"]),
-            request.form["output_csv_filename"],
+        config = Config(
+            imap_server_address=request.form["imap_server_address"],
+            imap_email_username=request.form["imap_email_username"],
+            imap_email_password=request.form["imap_email_password"],
+            imap_port=int(request.form["imap_port"]),
+            output_csv_filename=request.form["output_csv_filename"],
         )
-        return "Configuration saved! You can close this page."
+        # Tutaj możesz dodać kod do przetwarzania obiektu config,
+        # np. zapisać go do pliku lub użyć w dalszej części aplikacji.
+        return "Konfiguracja zapisana! Możesz zamknąć tę stronę."
     return render_template("configuration-site.html")
-    # return render_template_string()
 
 
 def run_flask():
+    """Runs flask configurator"""
     app.run(host="0.0.0.0", port=5000, debug=False)
 
 
